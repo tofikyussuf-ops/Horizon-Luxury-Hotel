bc78701bd97197d4f842c82073056e08d3056c62
Tofik <tofikyussuf@gmail.com>
2026-02-17 02:26:52 +0300

    feat: implement sign out action and personalized account greeting

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,16 +8,22 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  // 1. Tell Auth.js where your custom login page is
+  pages: {
+    signIn: "/login",
+  },
   callbacks: {
+    // 2. This is the "Security Guard" logic for Middleware
     authorized({ auth, request }) {
-      // Return true if the user is logged in (auth exists)
+      // Returns true (authorized) if there is a user, false if not
       return !!auth?.user;
     },
   },
 };
 
-// V5 exports these specific methods for you to use globally
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,18 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guest",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  // Get the first name by splitting the full name string
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, [Guest Name]
+      Welcome, {firstName}
     </h2>
   );
 }
